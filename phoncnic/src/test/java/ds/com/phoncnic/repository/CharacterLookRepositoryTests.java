e5535c086b1e3c9e0cb6eedad639bba0ca856f9a
heocat <bban777@gmail.com>
2022-03-01 18:01:19 +0900

    feat: 깃 레포지토리 이동, 전 레포지토리에 있던 파일 업로드

--- phoncnic/src/test/java/ds/com/phoncnic/repository/CharacterLookRepositoryTests.java
+++ phoncnic/src/test/java/ds/com/phoncnic/repository/CharacterLookRepositoryTests.java
@@ -1,5 +1,7 @@
 package ds.com.phoncnic.repository;
 
+
+import java.util.UUID;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
@@ -7,30 +9,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import ds.com.phoncnic.entity.CharacterLook;
+import ds.com.phoncnic.entity.CharacterLookInfo;
 import ds.com.phoncnic.entity.Member;
+import lombok.extern.log4j.Log4j2;
+
 
 @SpringBootTest
+@Log4j2
 public class CharacterLookRepositoryTests {
     @Autowired
     CharacterLookRepository repository;
 
+    @Autowired
+    private CharacterLookInfoRepository characterLookInforepository;
+    //ChracterLook 더미
     @Test
-    public void insertDummies() {
-        IntStream.rangeClosed(1, 10).forEach(
-                i -> {
-                    Member member = Member.builder()
-                            .id("user" + i + "@icloud.com").build();
+    public void insertDummies(){
 
-                    CharacterLook characterLook = CharacterLook.builder()
-                            .hair(i)
-                            .clothes(i)
-                            .member(member)
-                            .build();
+        IntStream.rangeClosed(1, 10).forEach(i->{
+            Long ch = ((long)(Math.random()*3+1));
 
-                    repository.save(characterLook);
-                }
+            Member member = Member.builder().id("user"+i+"@icloud.com").build();
+            CharacterLookInfo characterLookinfo = CharacterLookInfo.builder().chno(ch).build();
 
-        );
+            CharacterLook characterLook = CharacterLook.builder()
+            .member(member)
+            .characterLookinfo(characterLookinfo)
+            .hairname("hair"+ch)
+            .clothesname("clothes"+ch)
+            .build();
+            repository.save(characterLook);
 
+
+        });
+    }
+
+    //ChracterLookInfo 더미
+    @Test
+    public void insertChracterimg(){
+
+        IntStream.rangeClosed(1, 3).forEach(i->{
+            CharacterLookInfo characterLookinfo = CharacterLookInfo.builder()
+            .hairname("hair"+i)
+            .hairpath(UUID.randomUUID().toString())
+            .clothesname("clothes"+i)
+            .clothespath(UUID.randomUUID().toString())
+            .build();
+            characterLookInforepository.save(characterLookinfo);
+        });
+    }
+
+    @Test
+    public void TestGetCharacterLook() {
+    
+    CharacterLookInfo result = characterLookInforepository.getCharacterImgs("user1@icloud.com");
+    System.out.println(result.getHairpath());
+    System.out.println(result.getClothespath());
+
+    
     }
 }
