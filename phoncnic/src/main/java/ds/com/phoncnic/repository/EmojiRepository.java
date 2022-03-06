b6c1d172fa6d059382ac59590744a312af875789
JungGyeongmi <gm950715@gmail.com>
2022-03-06 19:21:23 +0900

    chore : (EmojiRepository) 및 (EmojiRepositoryTests) 수정

--- phoncnic/src/main/java/ds/com/phoncnic/repository/EmojiRepository.java
+++ phoncnic/src/main/java/ds/com/phoncnic/repository/EmojiRepository.java
@@ -1,11 +1,15 @@
 package ds.com.phoncnic.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import ds.com.phoncnic.entity.Emoji;
 
 public interface EmojiRepository extends JpaRepository<Emoji,Long>{
-    // 인자로 member id 를 받아서 emoji list를 출력하도록 
-    // @Query("select e from Emoji e LEFT OUTER JOIN EmojiInfo ei ON e.emojiInfo.emojitype = :ei.emojitype")
-    // List<Emoji> getEmojiByEmojiInfo(String emojitype);
+    // 인자로 member id 를 받아서 emoji list를 출력하도록
+    /* 이모지를 멤버가 마이페이지에서 확인할 때 사용할 것 */
+    @Query("select e from Emoji e where e.member.id=:id")
+    List<Emoji> getEmojiListByMember(String id);
 }
