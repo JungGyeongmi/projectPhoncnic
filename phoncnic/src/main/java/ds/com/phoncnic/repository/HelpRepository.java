30415f159724e7cf5584234998ed32aa0589471c
NagyeongHa <90600892+NagyeongHa@users.noreply.github.com>
2022-03-17 20:05:48 +0900

    feat : help /register , modify, remove

--- phoncnic/src/main/java/ds/com/phoncnic/repository/HelpRepository.java
+++ phoncnic/src/main/java/ds/com/phoncnic/repository/HelpRepository.java
@@ -1,5 +1,7 @@
 package ds.com.phoncnic.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +11,14 @@ import ds.com.phoncnic.entity.Help;
 
 public interface HelpRepository extends JpaRepository<Help, Long> {
 
-    @Query("select q from Help q where answerstatus = true or mod(qno, 2) = 1 ")
+    // @Query("select q from Help q where answerstatus = true or mod(qno, 2) = 1 ")
+    // Page<Help> getListPage(Pageable pageable);
+
+    @Query("select q from Help q where answerstatus = true ")
     Page<Help> getListPage(Pageable pageable);
+
+
+    // @Query("select q from Help q where qno= :qno ")
+    // List<Object[]> getHelpByQno(Long qno);
     
 }
