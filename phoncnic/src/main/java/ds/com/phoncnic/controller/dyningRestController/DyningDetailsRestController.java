cdb5b749e7d2ccd02d35bd3f2fcad9e055cce154
heocat <bban777@gmail.com>
2022-03-18 17:05:53 +0900

    feat:(DyningDetailsRestController)에서 이모지 삭제 기능 추가

--- phoncnic/src/main/java/ds/com/phoncnic/controller/dyningRestController/DyningDetailsRestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/dyningRestController/DyningDetailsRestController.java
@@ -1,11 +1,17 @@
 package ds.com.phoncnic.controller.dyningRestController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ds.com.phoncnic.dto.EmojiDTO;
 import ds.com.phoncnic.service.FollowService;
 import ds.com.phoncnic.service.dyning.DyningService;
 import ds.com.phoncnic.service.emoji.EmojiService;
@@ -30,12 +36,30 @@ public class DyningDetailsRestController {
         return "redirect:/dyning/details";
     }
 
+     //이모지 추가
+     @PostMapping("/dyning/emojiregister/{dno}")
+     public ResponseEntity<Long> addEmoji(@RequestBody EmojiDTO EmojiDTO) {
+         log.info("-----------------add Emoji-----------------");
+         log.info("EmojiDTO:" + EmojiDTO);
+ 
+         Long eno = emojiService.dyningEmojiRegister(EmojiDTO);
+         return new ResponseEntity<>(eno, HttpStatus.OK);
+         
+     }
+     //이모지 삭제
+     @DeleteMapping("/dyning/emojiremove/{eno}")
+     public ResponseEntity<Long> removeEmoji(@PathVariable Long eno) {
+         log.info("eno:" + eno);
+ 
+         emojiService.emojiRemove(eno);
+         return new ResponseEntity<>(eno, HttpStatus.OK);
+ 
+     }
+
     // 팔로우 추가
 
     // 팔로우 삭제
     // @DeleteMapping("/details")
 
-    // 이모지 추가
-
-    // 이모지 삭제
+   
 }
--- phoncnic/src/main/java/ds/com/phoncnic/controller/dyningRestController/DyningRestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/dyningRestController/DyningRestController.java
@@ -3,9 +3,10 @@ package ds.com.phoncnic.controller.dyningRestController;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ds.com.phoncnic.dto.EmojiDTO;
@@ -18,7 +19,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class DyningRestController {
     private final EmojiService emojiService;
-
+    //이모지 추가
     @PostMapping("/dyning/emojiregister/{dno}")
     public ResponseEntity<Long> addEmoji(@RequestBody EmojiDTO EmojiDTO) {
         log.info("-----------------add Emoji-----------------");
@@ -28,6 +29,14 @@ public class DyningRestController {
         return new ResponseEntity<>(eno, HttpStatus.OK);
         
     }
+    //이모지 삭제
+    @DeleteMapping("/dyning/emojiremove/{eno}")
+    public ResponseEntity<Long> removeEmoji(@PathVariable Long eno) {
+        log.info("eno:" + eno);
 
+        emojiService.emojiRemove(eno);
+        return new ResponseEntity<>(eno, HttpStatus.OK);
+
+    }
 }
 
