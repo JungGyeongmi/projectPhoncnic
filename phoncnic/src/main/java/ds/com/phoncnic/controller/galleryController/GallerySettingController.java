79525a68f1bec4f1a35fa03ecbffffd75e1f999f
unknown <gm950715@gmail.com>
2022-03-22 22:39:57 +0900

    chore : (GalleryController) 수정 -> 안쓰는 Service 삭제(주석), (GallerySettingController) remove의 redirect 주소 변경
    remove의 redirect 주소값이 잘못되서 user의 gallery list를 못불러왔음 ?="+id 에서 ?id="+id로 수정

--- phoncnic/src/main/java/ds/com/phoncnic/controller/galleryController/GallerySettingController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/galleryController/GallerySettingController.java
@@ -6,10 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ds.com.phoncnic.dto.GalleryDTO;
-import ds.com.phoncnic.dto.PageRequestDTO;
 import ds.com.phoncnic.service.emoji.EmojiInfoService;
 import ds.com.phoncnic.service.emoji.EmojiService;
 import ds.com.phoncnic.service.gallery.GalleryService;
@@ -46,11 +44,10 @@ public class GallerySettingController {
     public String getRegisetr(String id, GalleryDTO galleryDTO) {
         log.info("register...."+galleryDTO.getGno());
         galleryService.register(galleryDTO);
-        return "redirect:/main/mypage?id="+id;
+        return "redirect:/manage/gallery/list?id="+id;
     }
 
-    // read modify remove 모두 여기로 이동
- // read and modify
+    // read and modify
     @GetMapping({ "/read","/modify" })
     public void getReadPage(long gno, Model model) {
         model.addAttribute("emojiList", emojiService.getEmojiList("g", gno));
@@ -58,12 +55,6 @@ public class GallerySettingController {
         model.addAttribute("emojiInfoList", emojiInfoService.getEmojiInfoList());
         model.addAttribute("galleryDTO", galleryService.getGallery(gno));
     }
-
-    // read 후에 수정되는 부분을 어떻게 처리해야하는지
-    // @GetMapping({ "/modify" })
-    // public void getModifyPage(long gno, Model model) {
-    //     model.addAttribute("galleryDTO", galleryService.getGallery(gno));
-    // }
 
     @PostMapping("/modify")
     public String getRemovePage(GalleryDTO galleryDTO) {
@@ -78,7 +69,7 @@ public class GallerySettingController {
     public String getRemovePage(String id, long gno) {
         galleryService.removeWithEmojis(gno);
         log.info("remove page " + gno + "....");
-        return "redirect:/manage/gallery/list?"+id;
+        return "redirect:/manage/gallery/list?id="+id;
     }
 
 }
