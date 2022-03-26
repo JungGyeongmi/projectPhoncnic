97e3e750e992a79f27c4f38b7168fd0ab7cb04de
unknown <gm950715@gmail.com>
2022-03-26 17:23:50 +0900

    chore : (painting.list) 수정중 modal에 값 전달 가능 -> form으로 데이터 전달 작업중

--- phoncnic/src/main/java/ds/com/phoncnic/controller/galleryRestController/GalleryRestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/galleryRestController/GalleryRestController.java
@@ -16,22 +16,15 @@ import ds.com.phoncnic.service.gallery.GalleryService;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
+@RequestMapping("/gallery")
 @Log4j2
 public class GalleryRestController {
 
     @Autowired
     private GalleryService galleryService;
 
-    @GetMapping("/gallery/read/{gno}")
+    @GetMapping("/read/{gno}")
     public ResponseEntity<GalleryDTO> getRead(@PathVariable("gno") Long gno) {
-        log.info("post Gallery details......... rest "+gno);
-        GalleryDTO galleryDTO = galleryService.getGallery(gno);
-        log.info("galleryDTO : " + galleryDTO);
-        return new ResponseEntity<>(galleryDTO, HttpStatus.OK);
-    }
-
-    @GetMapping("/curator/test")
-    public ResponseEntity<GalleryDTO> getModalData(@PathVariable("gno") Long gno) {
         log.info("post Gallery details......... rest "+gno);
         GalleryDTO galleryDTO = galleryService.getGallery(gno);
         log.info("galleryDTO : " + galleryDTO);
@@ -39,9 +32,30 @@ public class GalleryRestController {
     }
     
     @GetMapping("/curator")
+    public ResponseEntity<PageResultDTO<GalleryDTO, Object[]>> getCuratorModal(SearchPageRequestDTO pageRequestDTO,
+        String type, String sort, String keyword) {
+
+        log.info("---------------get curator rest---------------");
+        log.info("type"+type);
+        log.info("sort"+sort);
+        log.info("keyword"+keyword);
+        pageRequestDTO.setType(type);
+        pageRequestDTO.setSort(sort);
+        pageRequestDTO.setKeyword(keyword);
+        PageResultDTO<GalleryDTO, Object[]> result = galleryService.getGalleryPage(pageRequestDTO);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping("/curator")
     public ResponseEntity<PageResultDTO<GalleryDTO, Object[]>> getSearchData(SearchPageRequestDTO pageRequestDTO,
         String type, String sort, String keyword) {
 
+
+        log.info("---------------post curator---------------");
+        log.info("type"+type);
+        log.info("sort"+sort);
+        log.info("keyword"+keyword);
         pageRequestDTO.setType(type);
         pageRequestDTO.setSort(sort);
         pageRequestDTO.setKeyword(keyword);
