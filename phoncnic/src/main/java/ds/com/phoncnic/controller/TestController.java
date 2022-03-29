b5f48089445ff57fef21726908c68c66e3756a52
NagyeongHa <90600892+NagyeongHa@users.noreply.github.com>
2022-03-29 23:22:13 +0900

    fix:slide 수정중

--- phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
@@ -1,10 +1,13 @@
 package ds.com.phoncnic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import ds.com.phoncnic.dto.pageDTO.PageRequestDTO;
+import ds.com.phoncnic.service.emoji.EmojiInfoService;
+import ds.com.phoncnic.service.emoji.EmojiService;
 import ds.com.phoncnic.service.gallery.GalleryService;
 import lombok.extern.log4j.Log4j2;
 
@@ -12,7 +15,10 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 public class TestController {
 
+    @Autowired
     private  GalleryService galleryService;
+    @Autowired
+    private EmojiInfoService emojiInfoService;
 
 
     @GetMapping("/test")
@@ -29,6 +35,7 @@ public class TestController {
     @GetMapping("/toggle")
     public String painting( PageRequestDTO pageRequestDTO, Model model, Long gno){
         model.addAttribute("galleryDTOList", galleryService.getGalleryList(true));
+        model.addAttribute("emojiInfoList", emojiInfoService.getEmojiInfoList());
         model.addAttribute("list", galleryService.getPaintingList(pageRequestDTO));
         return "toggleTest";
     }
