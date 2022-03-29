b318b633e3df2c91d87388b1efbeff1d180b93b5
NagyeongHa <90600892+NagyeongHa@users.noreply.github.com>
2022-03-29 18:45:17 +0900

    chore: slide 수정중

--- phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
@@ -1,18 +1,36 @@
 package ds.com.phoncnic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ds.com.phoncnic.dto.pageDTO.PageRequestDTO;
+import ds.com.phoncnic.service.gallery.GalleryService;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Controller
 public class TestController {
 
+    private  GalleryService galleryService;
+
+
     @GetMapping("/test")
     public String test () {
         log.info("test....");
         return "test";
+    }
+
+    // @GetMapping("/toggle")
+    // public String slide(){
+    //     return "toggleTest";
+    // }
+
+    @GetMapping("/toggle")
+    public String painting( PageRequestDTO pageRequestDTO, Model model, Long gno){
+        model.addAttribute("galleryDTOList", galleryService.getGalleryList(true));
+        model.addAttribute("list", galleryService.getPaintingList(pageRequestDTO));
+        return "toggleTest";
     }
 
 }
