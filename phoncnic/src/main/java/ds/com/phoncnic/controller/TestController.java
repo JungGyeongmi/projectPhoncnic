62754ef8f422849a6dc06ad3b1cebb29af74c75c
miiiiin1020 <miiiiin1020@gmail.com>
2022-03-30 16:38:32 +0900

    chore: 캐릭터룩 모달 테스트중
    chore: 캐릭터룩 모달 테스트중

--- phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
@@ -1,14 +1,28 @@
 package ds.com.phoncnic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ds.com.phoncnic.service.mypage.MemberService;
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 public class TestController {
+
+    // private final MemberService memberService;
 
     @GetMapping("/test")
     public String test () {
         return "test";
     }
+
+    // @GetMapping("/test2")
+    // public String test2 (String id, Model model) {
+    //     log.info("id: "+id);
+    //     MemberDTO
+    //     return "test2";
+    // }
 
 }
