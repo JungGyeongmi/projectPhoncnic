6d2182c6986b8318ebfc5828e3c27e273946b375
heocat <bban777@gmail.com>
2022-03-31 10:19:38 +0900

    chore:(chracterlook)모달에서 수정가능

--- phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/TestController.java
@@ -3,13 +3,19 @@ package ds.com.phoncnic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import ds.com.phoncnic.service.mypage.MemberService;
+import ds.com.phoncnic.dto.CharacterLookDTO;
+import ds.com.phoncnic.service.mypage.CharacterLookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @Log4j2
+@RequiredArgsConstructor
 public class TestController {
+    private final CharacterLookService characterLookService;
 
     // private final MemberService memberService;
 
@@ -19,9 +25,15 @@ public class TestController {
     }
 
     @GetMapping("/test2")
-    public String test2 () {
-        
-        return "test2";
+    public String test2 (String id, Model model) {
+        log.info("id:" + id);
+        model.addAttribute("id",id);
+        model.addAttribute("hairDTO", characterLookService.getCharacterHair(id));
+        model.addAttribute("clothesDTO", characterLookService.getCharacterClothes(id));
+        return "/test2";
     }
 
+  
 }
+
+ 
