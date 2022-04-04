e960ac0e47769f59cdfe4f08c49b000c8fe08c81
miiiiin1020 <miiiiin1020@gmail.com>
2022-04-04 09:32:54 +0900

    chore: dyninglist 왼쪽 검색 토글, 오른쪽 메뉴 드롭다운

--- phoncnic/src/main/java/ds/com/phoncnic/controller/TestRestController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/TestRestController.java
@@ -11,30 +11,29 @@ import ds.com.phoncnic.dto.pageDTO.PageResultDTO;
 import ds.com.phoncnic.dto.pageDTO.SearchPageRequestDTO;
 import ds.com.phoncnic.service.gallery.GalleryService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Log4j2
 @RestController
 public class TestRestController {
-    
-    
+
     @Autowired
     private GalleryService galleryService;
 
-    @GetMapping("/member/login") 
-    public String login () {
+    @GetMapping("/member/login")
+    public String login() {
         log.info("login page.....");
         return "/member/login";
     }
 
-
     @GetMapping("/api/test")
     public ResponseEntity<PageResultDTO<GalleryDTO, Object[]>> getSearchData(SearchPageRequestDTO pageRequestDTO,
-        String type, String sort, String keyword) {
+            String type, String sort, String keyword) {
 
         pageRequestDTO.setType(type);
         pageRequestDTO.setSort(sort);
         pageRequestDTO.setKeyword(keyword);
-        
+
         PageResultDTO<GalleryDTO, Object[]> result = galleryService.getGalleryPage(pageRequestDTO);
 
         log.info("gallery rest controller ... curator ...");
@@ -42,5 +41,4 @@ public class TestRestController {
         log.info(result.getDtoList());
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
-
 }
