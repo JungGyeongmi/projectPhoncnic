93a9af76de1ef4190fcc7e1073f39e33860d832a
heocat <bban777@gmail.com>
2022-04-07 17:20:53 +0900

    chore:jsp 디펜던시추가

--- phoncnic/src/main/java/ds/com/phoncnic/PhoncnicApplication.java
+++ phoncnic/src/main/java/ds/com/phoncnic/PhoncnicApplication.java
@@ -2,10 +2,12 @@ package ds.com.phoncnic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan
 public class PhoncnicApplication {
 
 	public static void main(String[] args) {
