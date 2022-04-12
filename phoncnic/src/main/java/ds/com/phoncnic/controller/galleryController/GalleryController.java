5ed95abf35426d6accc1484581599f8a71fa7e34
JungGyeongmi <gm950715@gmail.com>
2022-04-13 02:27:59 +0900

    chore: gallery (listh.html) characterlookmodal 추가 수정 가능

--- phoncnic/src/main/java/ds/com/phoncnic/controller/galleryController/GalleryController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/galleryController/GalleryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import ds.com.phoncnic.dto.pageDTO.PageRequestDTO;
 import ds.com.phoncnic.security.dto.AuthMemberDTO;
+import ds.com.phoncnic.service.characterLook.CharacterLookService;
 import ds.com.phoncnic.service.gallery.GalleryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -20,6 +21,8 @@ import lombok.extern.log4j.Log4j2;
 public class GalleryController {
 
     private final GalleryService galleryService;
+
+    private final CharacterLookService characterLookService;
 
     @GetMapping({ "/", "" })
     public String galleryCrossroad() {
@@ -39,12 +42,17 @@ public class GalleryController {
     public String photo(PageRequestDTO pageRequestDTO, Model model, Long gno, @AuthenticationPrincipal AuthMemberDTO authMemberDTO) {
         model.addAttribute("galleryDTOList", galleryService.getGalleryList(false));
         model.addAttribute("list", galleryService.getPhotoList(pageRequestDTO));
-        String loginUserId = "";
+
         if(authMemberDTO != null) {
+            String loginUserId = authMemberDTO.getId();
             log.info(authMemberDTO);
-            loginUserId = authMemberDTO.getId();
+            model.addAttribute("loginuserId", loginUserId);
+            model.addAttribute("avatar", characterLookService.getCharacterSet(loginUserId));
+            model.addAttribute("loginUserId", loginUserId);
+        } else {
+            model.addAttribute("avatar", characterLookService.getDefaultAvatar(1L));
         }
-        model.addAttribute("loginUserId", loginUserId);
+
         return "gallery/photo/list";
     }
 
@@ -53,12 +61,16 @@ public class GalleryController {
     public String painting(PageRequestDTO pageRequestDTO, Model model, Long gno, @AuthenticationPrincipal AuthMemberDTO authMemberDTO) {
         model.addAttribute("galleryDTOList", galleryService.getGalleryList(true));
         model.addAttribute("list", galleryService.getPaintingList(pageRequestDTO));
-        String loginUserId = "";
+        
         if(authMemberDTO != null) {
             log.info(authMemberDTO);
-            loginUserId = authMemberDTO.getId();
+            String loginUserId = authMemberDTO.getId();
+            model.addAttribute("loginuserId", loginUserId);
+            model.addAttribute("avatar", characterLookService.getCharacterSet(loginUserId));
+            model.addAttribute("loginUserId", loginUserId);
+        } else {
+            model.addAttribute("avatar", characterLookService.getDefaultAvatar(1L));
         }
-        model.addAttribute("loginUserId", loginUserId);
         return "gallery/painting/list";
     }
 }
