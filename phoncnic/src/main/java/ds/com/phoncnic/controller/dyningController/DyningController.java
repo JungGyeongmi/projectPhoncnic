856d9d26c75d7aed51e1688883e2bf55595e2dfd
JungGyeongmi <gm950715@gmail.com>
2022-04-14 18:57:36 +0900

    chore: merge 후 충돌 수정

--- phoncnic/src/main/java/ds/com/phoncnic/controller/dyningController/DyningController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/dyningController/DyningController.java
@@ -1,9 +1,5 @@
 package ds.com.phoncnic.controller.dyningController;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
