a4c084626e4081e025c861bbdcab31a411241f4d
JungGyeongmi <gm950715@gmail.com>
2022-04-16 18:01:40 +0900

    chore: (ReceptionController.java) (artistForm.html) 수정 중

--- phoncnic/src/main/java/ds/com/phoncnic/controller/ReceptionController.java
+++ phoncnic/src/main/java/ds/com/phoncnic/controller/ReceptionController.java
@@ -1,29 +1,40 @@
 package ds.com.phoncnic.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ds.com.phoncnic.dto.ApplicationFormDTO;
+import ds.com.phoncnic.security.dto.AuthMemberDTO;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 @RequestMapping("/reception")
 public class ReceptionController {
 
   @GetMapping("/ceo")
-  public String toCeoForm() {
+  public String toCeoForm(@AuthenticationPrincipal AuthMemberDTO authDTO, Model model) {
     
+    model.addAttribute("authDTO", authDTO);
     return "/application/ceoForm";
   }
   
   @GetMapping("/artist")
-  public String toArtistForm() {
+  public String toArtistForm(@AuthenticationPrincipal AuthMemberDTO authDTO, Model model) {
     
+    model.addAttribute("authDTO", authDTO);
     return "/application/artistForm";
   }
   
-  @PostMapping("/artist/submit")
-  public String submitArtist() {
+  @PostMapping("/register")
+  public String registerForm(ApplicationFormDTO applyDTO) {
     
+    log.info("apply register....");
+    log.info("applyDTO..."+applyDTO);
     return "redirect:/reception";
   }
 }
