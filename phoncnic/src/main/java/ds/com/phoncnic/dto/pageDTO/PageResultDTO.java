c97ea7ba91f3a87c25f5b5d3b9a4d02486859c4e
Jung Gyeongmi <gm950715@gmail.com>
2022-04-18 12:41:26 +0900

    chore: (pageResultDTO) 수정 pageList뜸

--- phoncnic/src/main/java/ds/com/phoncnic/dto/pageDTO/PageResultDTO.java
+++ phoncnic/src/main/java/ds/com/phoncnic/dto/pageDTO/PageResultDTO.java
@@ -11,31 +11,34 @@ import org.springframework.data.domain.Pageable;
 import lombok.Data;
 
 @Data
+//페이지요청의 결과를 담기 위한 클래스
 public class PageResultDTO<DTO, EN> {
-    private List<DTO> dtoList;
+  //page 결과목록을 담기 위한 변수
+  private List<DTO> dtoList;
 
-    private int page;
-    private int size; 
-    private int totalPage; 
-    private int start, end; 
-    private boolean prev, next; 
-    private List<Integer> pageList;
+  //page navigation을 위한 변수들
+  private int page;
+  private int size;
+  private int totalPage;
+  private int start, end;
+  private boolean prev, next;
+  private List<Integer> pageList;
+  
+  public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
+    dtoList = result.stream().map(fn).collect(Collectors.toList());
+    totalPage = result.getTotalPages();
+    makePageList(result.getPageable());
+  }
 
-    // 임시 수정
-    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
-        dtoList = result.stream().map(fn).collect(Collectors.toList());
-        totalPage = result.getTotalPages();
-        makePageList(result.getPageable());
-    }
-
-    private void makePageList(Pageable pageable) {
-        this.page = pageable.getPageNumber() + 1;
-        this.size = pageable.getPageSize();
-        int tempEnd = (int) (Math.ceil(page / (double) size)) * 10;
-        start = tempEnd - (size - 1);
-        end = totalPage > tempEnd ? tempEnd : totalPage;
-        prev = start > 1;
-        next = totalPage > tempEnd;
-        pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
-    }
+  private void makePageList(Pageable pageable){
+    this.page = pageable.getPageNumber()+1;
+    this.size = pageable.getPageSize();
+    int tempEnd = (int)(Math.ceil(page/(double)size))*size;
+    start = tempEnd-(size-1);
+    end = totalPage > tempEnd ? tempEnd : totalPage;
+    prev = start>1;
+    next = totalPage > tempEnd;
+    pageList = IntStream.rangeClosed(start, end)
+      .boxed().collect(Collectors.toList());
+  }
 }
