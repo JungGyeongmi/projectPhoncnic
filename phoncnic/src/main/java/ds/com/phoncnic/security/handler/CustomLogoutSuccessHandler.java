406c93ae8471adfd3ac581fab11759eb74029437
JungGyeongmi <gm950715@gmail.com>
2022-04-19 16:39:40 +0900

    chore : security config 수정

--- phoncnic/src/main/java/ds/com/phoncnic/security/handler/CustomLogoutSuccessHandler.java
+++ phoncnic/src/main/java/ds/com/phoncnic/security/handler/CustomLogoutSuccessHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -14,35 +15,34 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
   @Override
   public void onLogoutSuccess(
-    HttpServletRequest request, 
-    HttpServletResponse response, 
-    Authentication auth)
-    throws IOException, ServletException {
+      HttpServletRequest request,
+      HttpServletResponse response,
+      Authentication auth)
+      throws IOException, ServletException {
 
     log.warn("logoutsuccess...");
     
-    if(auth != null && auth.getDetails() != null){
-      log.info("auth.getDetails : "+auth.getDetails());
-      try {        
+    Cookie myCookie = new Cookie("JSESSIONID", "");
+    myCookie.setMaxAge(0);
+    myCookie.setPath("/"); // 모든 경로에서 접근 가능 하도록 설정
+    response.addCookie(myCookie);
+    
+    if (auth != null && auth.getDetails() != null) {
+      log.info("auth.getDetails : " + auth.getDetails());
+      try {
         request.getSession().invalidate();
+
         log.info("logout invalidate....");
-      } catch (Exception e) {e.printStackTrace();}
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
       response.setStatus(HttpServletResponse.SC_OK);
-      response.sendRedirect(request.getContextPath()+"/");
+      response.sendRedirect(request.getHeader("Referer"));
     }
   }
 
-  public static Cookie getCookie(HttpServletRequest request,String name) {
-    Cookie[] cookies = request.getCookies();
-    if(cookies!=null) {
-      for (Cookie cookie : cookies) {
-        if(cookie.getName().equals(name)) {
-          return cookie;
-        }
-      }
-    }
-    return null;
-  }
+  
 }
