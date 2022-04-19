4468363ac31afbab969e40ddbc69805bb31309d8
heocat <bban777@gmail.com>
2022-04-19 09:48:59 +0900

    chore:외부 로컬 호스트 로그인 안되는 문제

--- phoncnic/src/main/java/ds/com/phoncnic/security/handler/CustomLoginSuccessHandler.java
+++ phoncnic/src/main/java/ds/com/phoncnic/security/handler/CustomLoginSuccessHandler.java
@@ -51,7 +51,8 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
         session.removeAttribute("prevPage");
       } else {
         log.info("boboobobobo");
-        response.sendRedirect("https://28c0-175-119-172-242.jp.ngrok.io/phoncnic/");
+        log.info("request.getContextPath()---"+request.getContextPath());
+        response.sendRedirect(request.getContextPath()+"/");
       }
     }
   }
